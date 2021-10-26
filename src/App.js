f53c2232cddfa1d6d12b602cb3e3a420f30980cc
jdias <jdias@direct.one>
2021-10-26 13:12:02 -0300

    Update 26102021

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import { Filter } from "./components/Filter/Filter";
+import { TopMenu } from "./components/TopMenu/TopMenu";
+import { DenseTable } from "./components/Table/Table";
+import './App.css'
 function App() {
   return (
-    <div className="App">
-     <h1>Sicredi</h1>
-    </div>
+    <>
+      <TopMenu/>
+      <Filter/>
+      <br/>
+      <br/>
+      <DenseTable/>
+    </>
   );
 }
 
